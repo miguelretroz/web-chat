df8ac3e29d565372bacb6aac3b44ad375447a749
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-20 16:35:52 -0300

    Store the messages in the database

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,9 +1,12 @@
 const moment = require('moment');
+const { MessagesModel } = require('../models');
 
 module.exports = (io) => io.on('connection', (socket) => {
     console.log(`Usuário ${socket.id} se conectou!`);
 
-    socket.on('message', ({ chatMessage, nickname }) => {
+    socket.on('message', async ({ chatMessage, nickname }) => {
+      await MessagesModel.create({ message: chatMessage, nickname });
+
       io.emit('message', `${moment().format('DD-MM-yyyy HH:mm:ss')} ${nickname} ${chatMessage}`);
     });
 });
