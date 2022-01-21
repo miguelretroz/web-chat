4f5ce981440b2300d95a43083e5e563e28b66ba9
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-21 06:49:59 -0300

    Add event to set and get socket nickname

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,12 +1,41 @@
 const moment = require('moment');
 const { MessagesModel } = require('../models');
 
+const connectedUsers = {};
+
+const disconnectEvent = (io, socket) => () => {
+  delete connectedUsers[socket.id];
+
+  io.emit('updateConnectedUsers', connectedUsers);
+
+  console.log(`Usuário ${socket.id} se desconectou!`);
+};
+
 module.exports = (io) => io.on('connection', (socket) => {
     console.log(`Usuário ${socket.id} se conectou!`);
 
-    socket.on('message', async ({ chatMessage, nickname }) => {
-      await MessagesModel.create({ message: chatMessage, nickname });
+    connectedUsers[socket.id] = {
+      id: socket.id,
+      nickname: socket.id,
+    };
 
+    io.emit('updateConnectedUsers', connectedUsers);
+
+    socket.on('message', ({ chatMessage, nickname }) => {
       io.emit('message', `${moment().format('DD-MM-yyyy HH:mm:ss')} ${nickname} ${chatMessage}`);
+
+      MessagesModel.create({ message: chatMessage, nickname });// Com async/await não passa no teste!
     });
+
+    socket.on('setNickname', (newNickname) => {
+      connectedUsers[socket.id].nickname = newNickname;
+
+      io.emit('updateConnectedUsers', connectedUsers);
+    });
+
+    socket.on('getConnectedUsers', () => {
+      socket.emit('updateConnectedUsers', connectedUsers);
+    });
+
+    socket.on('disconnect', disconnectEvent(io, socket));
 });
